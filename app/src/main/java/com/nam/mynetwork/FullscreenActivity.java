76d0f9c445ec351cdd2d05412e0804d2fbd101cf
hp <namstudionsk@gmail.com>
2018-01-14 22:10:30 +0700

    Networking URL Done

--- app/src/main/java/com/nam/mynetwork/FullscreenActivity.java
+++ app/src/main/java/com/nam/mynetwork/FullscreenActivity.java
@@ -1,18 +1,23 @@
 package com.nam.mynetwork;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class FullscreenActivity extends AppCompatActivity {
+
+    private Button mButton;
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -106,6 +111,16 @@ public class FullscreenActivity extends AppCompatActivity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FullscreenActivity.this, NetworkingURLActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
